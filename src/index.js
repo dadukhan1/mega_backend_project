72f537ff98e66e1b1e1a41649c8b24c5e5c9e41d
Dadu Khan <photoshopt65@gmail.com>
2025-01-24 10:03:08 -0800

    utils, error handling

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 // require("dotenv").config({ path: "./env" }) 
 import dotenv from 'dotenv';
 import connectDB from './db/index.js';
+import { app } from './app.js';
 
 dotenv.config({
     path: "./env"
-}) 
+})
 
 
 connectDB()
+    .then(() => {
+
+        app.on("error", (err) => {
+            console.log("Erorr : ", err)
+        })
+
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`App is listening on port ${process.env.PORT}`)
+        })
+    })
+    .catch((err) => {
+        console.log("Database connection failed : ", err)
+    })
 
 
 
